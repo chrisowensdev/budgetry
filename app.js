7b67c330d51927b4c515c4cdd0607b2eed445150
guitarrockstarco <guitarrockstarco@yahoo.com>
2020-02-28 07:28:44 -0500

    Add unique id for each inc or exp

--- app.js
+++ app.js
@@ -1,6 +1,57 @@
 //BUDGET CONTROLLER
 var budgetController = (function() {
-  //Some Code
+  var Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  };
+
+  return {
+    addItem: function(type, des, val) {
+      var newItem, ID;
+
+      //Create new ID
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      //Create new item based on 'inc' or 'exp' type
+      if (type === 'exp') {
+        newItem = new Expense(ID, des, val);
+      } else if (type === 'inc') {
+        newItem = new Income(ID, des, val);
+      }
+
+      // Push it into our data structure
+      data.allItems[type].push(newItem);
+
+      //Return the new element
+      return newItem;
+    },
+
+    testing: function() {
+      console.log(data);
+    }
+  };
 })();
 
 //UI CONTROLLER
@@ -15,7 +66,7 @@ var UIController = (function() {
     getInput: function() {
       return {
         type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
-        desription: document.querySelector(DOMstrings.inputDescription).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
         value: document.querySelector(DOMstrings.inputValue).value
       };
     },
@@ -39,9 +90,14 @@ var contoller = (function(budgetCtrl, UICtrl) {
   };
 
   var ctrlAddItem = function() {
+    var input, newItem;
+
     // 1. Get the field input data
-    var input = UICtrl.getInput();
+    input = UICtrl.getInput();
+
     // 2. Add the item to the budget controller
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
     // 3. Add the item to the UI
     // 4. Calculate the budget
     // 5. Display the budget on UI
